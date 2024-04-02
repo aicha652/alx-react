b55850e3e2f9ce5dcbbacd87f54423d71a89f958
aicha652 <aichaazlf652@gmail.com>
2024-04-02 15:13:31 +0000

    fix

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,11 +2,9 @@ import $ from "jquery";
 $(document).ready(function(){
 	$('body').append('<p>Holberton Dashboard</p>');
 	$('body').append('<p>Dashboard data for the students</p>');
-	$('body').append('<button id="clickButton">Click here to get started</button>');
+	$('body').append('<button>Click here to get started</button>');
 	$('body').append('<p id="count"></p>');
 	$('body').append('<p>Copyright - Holberton School</p>');
-
-	$('#clickButton').click(updateCounter);
 });
 
 function updateCounter(){
