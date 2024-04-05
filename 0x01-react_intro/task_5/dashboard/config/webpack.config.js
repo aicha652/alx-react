4c84840d45fe0de47174ff6bdf1842242120a44e
aicha652 <aichaazlf652@gmail.com>
2024-04-05 15:35:38 +0200

    Create a project using Webpack

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	},
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, '../dist'),
     hot: true
   },
   plugins: [
