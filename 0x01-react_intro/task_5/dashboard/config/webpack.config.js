6a8abf0d2728088aae4eb5f22f2e5ac5b2d705f8
aicha652 <aichaazlf652@gmail.com>
2024-04-05 14:41:31 +0200

    Create a project using Webpack

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,43 +1,42 @@
 const path = require('path');
 
 module.exports = {
-	mode: 'production',
 	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        port: 9002,
-    },
-    resolve: {
-        extensions: ['.ts', ".js"],
-    },
-    module: {
-        rules: [
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9002,
+  },
+  resolve: {
+    extensions: ['.ts', ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
           {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
           },
-          {
-            test: /\.js$/,
-            enforce: "pre",
-            use: ["source-map-loader"],
-          },
-          {
-            test: /\.(gif|png|jpe?g|svg)$/i,
-            use: [
-              'file-loader',
-              {
-                loader: 'image-webpack-loader',
-                options: {
-                  bypassOnDebug: true, // webpack@1.x
-                  disable: true, // webpack@2.x and newer
-                },
-              },
-            ],
-          }
         ],
-    },
+      }
+    ],
+  },
 };
