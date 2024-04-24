a261dc1c81b422bf07baca304ba393534ece27f6
aicha652 <aichaazlf652@gmail.com>
2024-04-24 17:56:48 +0200

    task 3

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -5,7 +5,5 @@ export default function getListObject(array) {
     return List(array);
 }
 export function addElementToList(list, element) {
-
-    list.push(element);
-    return list;
+    return list.push(element);
 }
