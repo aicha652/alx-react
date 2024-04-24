41d320002f9cede7934f91da01c0a3e7af44d903
aicha652 <aichaazlf652@gmail.com>
2024-04-24 16:51:48 +0200

    task 3

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,11 +1,11 @@
-const { List } = require("immutable");
+import { List } from 'immutable';
 
-export function getListObject(array) {
+export default function getListObject(array) {
 
     return List(array);
 }
 
-export function addElementToList(list, element) {
+export default function addElementToList(list, element) {
 
     list.push(element);
     return list;
