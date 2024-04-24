c76b1acf786ba99110f2a83e1cc455164e9e2e69
aicha652 <aichaazlf652@gmail.com>
2024-04-24 21:19:08 +0200

    task 4

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -9,4 +9,6 @@ export const map = Immutable.Map({
     6: 'Lucas',
 });
 
-export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver')
+export const map2 = map.withMutations(() => {
+    map.set(2, 'Benjamin').set(4, 'Oliver')
+});
