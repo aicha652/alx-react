521ea959a3ebfb98a350852a11f68e59a6ebea1d
aicha652 <aichaazlf652@gmail.com>
2024-04-24 18:02:53 +0200

    task 5

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,7 +1,7 @@
 import { List } from 'immutable';
 import { Map } from 'immutable';
 
-export default function concatElements(page1, page2) {
+export function concatElements(page1, page2) {
     const page = page1.concat(page2);
     return List(page);
 }
